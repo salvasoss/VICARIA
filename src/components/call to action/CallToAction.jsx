1bb6d98f7a6aaddd6c2b5fdef1607cf7a7381fa4
Salvador Sosa <salvasoss@icloud.com>
2024-07-07 17:27:18 -0300

    acomodar productos index

--- src/components/call to action/CallToAction.jsx
+++ src/components/call to action/CallToAction.jsx
@@ -11,7 +11,7 @@ export const CallToAction = () => {
             <div className="callToAction">
                 <div className="content">
                     <h2> CALIDAD. EXPERIENCIA. RENDIMIENTO.</h2>
-                    <h3> Sella Grietas Vicaria, desde <b> 1954 </b>, con mas de <b> 70 </b> años en el mercado y con los mejores precios <b> Mayoristas </b> y <b>Minoristas </b> del pais! </h3>
+                    <h3> <b> Sella Grietas</b> Vicaria, desde <b> 1954 </b>, con mas de <b> 70 </b> años en el mercado y con los mejores precios <b> Mayoristas </b> y <b>Minoristas </b> del pais! Productos elaborados con materia prima importada de alta calidad  </h3>
                     <button className="contactButton"> PRECIOS MAYORISTAS/MINORISTAS</button>
                 </div>
 
